ed23fac5b1d4f617a225d932d8c13c4413a15489
juhidesai <juhi.nmims@gmail.com>
2015-02-16 19:46:03 -0500

    changes to automate-errors still exist

--- automated.py
+++ automated.py
@@ -17,10 +17,10 @@ cov = coverage()
 import fnmatch
 ##
 i=0
-res = []
+res = {}
 listmethod=['runA','runAB','runB']
 total_failed = 0
-score =[1,2,3]
+score ={}#[1,2,3]
 for file in os.listdir():
     if fnmatch.fnmatch(file, 'test_*.py'):
         print(file)
@@ -38,53 +38,11 @@ for file in os.listdir():
         print("result :"+str(result))
         cov.stop()
 ##        print("score: "+str(score))
-        res=cov.analysis('test3.py')
+        res[i]=cov.analysis('test3.py')
         print(res)
         i+=1
 print("score: "+str(score))
-####        globals[f] = __import__(f)
-##import 
-
-##from test import test_a
-##test.test_a.runA()
-##list1 = ['test_a.py','test_b']#'test_a.py'
-##loader = importlib.machinery.SourceFileLoader('mmm', list1)
-##mod = loader.load_module()
-##print(mod)
-##modules=map(__import__,list1)
-##importlib.import_module(list1)
-##print(x[2])
-##mod.runA()
-####test1.runTests()
-##score_a = mod.runA()
-##
-##
-##
-##
-##
-##cov.stop()
-##results = cov.analysis('test3.py')
-##cov.annotate('test3.py')
-##cov.html_report(directory='testA')
-##cov.erase()
-##
-##cov.start()
-##score_b = mod.runB()
-##cov.stop()
-##results1 = cov.analysis('test3.py')
-##cov.annotate()
-##print(results)
-##print(results1)
-##cov.html_report(directory='testB')
-##
-##cov.erase()
-##cov.start()
-##score_ab=mod.runAB()
-##cov.stop()
-##results2=cov.analysis('test3.py')
-##print(results2)
-##
-##calculate suspiciousness
+print("res: "+str(res))
 
 ##total_failed = score_a + score_b +score_ab
 for x in range(0,len(score)):
@@ -93,38 +51,53 @@ total_tests = len(res)
 total_passed = total_tests - total_failed
 passed = 0
 
-print(total_failed)
-
-susp = str(res[1])
-print(susp)
+print("tot failed: "+str(total_failed))
+susp={}
+susp = res[1]
+print("susp: "+str(susp))
+print("res1"+str(res[1][1]))
 i = 0
-for x in res[1]:
+for x in res[1][1]:
     failed_cases = 0
     passed=0
     # for runA
 ##    print(str(x))
-    if x not in res[2]:
-        if score[0] == 1:
-            failed_cases += 1
-        else:
-            passed+=1
-
-    if x not in res[2]:
-        if score[1] == 1:
-            failed_cases += 1
-        else:
-            passed+=1
-            
-    if x not in res[2]:
-        if score[2] == 1:
-            failed_cases += 1
-        else:
-            passed+=1
+    for y in range(1,len(res)):
+        print(str(y))
+        print(res[y][2])
+        if x not in res[y][2]:
+            if score[y] == 1:
+                failed_cases+=1
+            else:
+                passed+=1
+##
+##    if x not in res[1][2]:
+##        if score[0] == 1:
+##            failed_cases += 1
+##        else:
+##            passed+=1
+##
+##    if x not in res[1][2]:
+##        if score[1] == 1:
+##            failed_cases += 1
+##        else:
+##            passed+=1
+##            
+##    if x not in res[2]:
+##        if score[2] == 1:
+##            failed_cases += 1
+##        else:
+##            passed+=1
         
-    print("failed_cases" + str(failed_cases) + " for line "+str(x))
-    print("passed" + str(passed) + " for line "+str(x))
+        print("failed_cases" + str(failed_cases) + " for line "+str(x))
+        print("passed" + str(passed) + " for line "+str(x))
     ##passed = total_passed - failed_cases
-    susp[i] = str((failed_cases / total_failed ) / ((passed/total_passed)+(failed_cases / total_failed )))
+        if (passed/total_passed)==0 and (failed_cases/total_failed)==0 :
+            #or total_passed==0
+            print("division by zero!!!!")
+        else :
+            
+            susp[i] = (failed_cases / total_failed ) / ((passed/total_passed)+(failed_cases / total_failed ))
     i+=1
 
 print(str(susp))
