9db3f2a782cdd4b9594e6475643a3b8527d4c7a4
keemen90 <akeemeagles@gmail.com>
2015-02-18 22:39:32 -0500

    changed test1 to prototype

--- faultLocalization/Test/prototype.py
+++ faultLocalization/Test/prototype.py
@@ -1,8 +1,26 @@
 import trace
-from recurse import recurse
+import string, os, sys
+from Testrecurse import recurse
 
 tracer = trace.Trace(count=True, trace=False)
 tracer.runfunc(recurse, 2)
 
 results = tracer.results()
 results.write_results(coverdir='output')
+
+files = os.listdir('.\output')
+outputfile = open(os.path.join('output', 'recurse.ftest') , 'w')
+for f in files: 
+    if f[-5:] == 'cover':
+		outputfile.write(os.path.realpath(f) + '\n\n')
+		inputfile = open(os.path.join('output', f), 'r')
+		for line in inputfile: 
+			subline = line[:6]
+			if ':' in subline:
+				newline = '+ ' + line
+			elif '>>>>>>' in subline:
+				newline = '- ' + line
+			else:
+				newline = line
+			outputfile.write(newline)
+        
