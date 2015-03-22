770de9e4fb3ad720a659d6f7e469c566cfa93cb6
juhidesai <juhi.nmims@gmail.com>
2015-03-22 17:32:31 -0400

    multiple functions in one test file

--- test/test_a1.py
+++ test/test_a1.py
@@ -1,13 +1,20 @@
+import sys
 import unittest
 import os
+
+p = os.path.join(os.getcwd(),'src')
+print("@@@@@@@@@@@@@@@")
+print(p)
+sys.path.insert(0,p)
 import test3
+import test4
 ##import abc
 
 ##class DemoTest(unittest.TestCase):
 
-def runA():
-    print(os.getcwd())
-    return test_a()
+##def runA():
+##    print(os.getcwd())
+##    return test_a()
 
 
 
@@ -19,6 +26,25 @@ def test_a():
     except AssertionError:
         print("test_a screwed up!!")
         return 1
+
+def test_a_copy():
+    try:
+        assert test3.a() == True
+        return 0
+    except AssertionError:
+        print("test_a screwed up!!")
+        return 1
+
+def test_c():
+    try:
+        num = 6
+        print("in test_c :",test4.c(6))
+        assert test4.c(6) == 6
+        return 0
+    except:
+        print("test_c screwed up")
+        return 1
+
 
 
 
